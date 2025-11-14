9adec24bbf24432386fc795d8abed6f76801d749
Clem12358 <clement.durix@gmail.com>
2025-11-14 21:52:00 +0100

    Update 3_Gates.py

--- pages/3_Gates.py
+++ pages/3_Gates.py
@@ -3,9 +3,18 @@ from modules.sonnendach import fetch_address_data
 
 st.markdown("""
     <style>
-    .stPageLink {display: none !important;}
+        /* Hide Streamlit’s default sidebar */
+        section[data-testid="stSidebar"] {
+            display: none !important;
+        }
+
+        /* Expand main content to full width */
+        div[data-testid="stAppViewContainer"] {
+            margin-left: 0 !important;
+        }
     </style>
-    """, unsafe_allow_html=True)
+""", unsafe_allow_html=True)
+
 
 
 st.title("Pass/Fail Gates")
